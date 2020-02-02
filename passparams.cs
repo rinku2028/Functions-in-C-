b052045e6bf22ab2646f663df5b7354019703e2e
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-02 23:54:39 +0545

    Add files via upload

---
+++ passparams.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace helloworld
+{
+    class square
+    {
+        static void calcsquare(int x,int y)
+        {
+            x = x * x;
+            y = y * y;
+            Console.WriteLine(x + "\t" + y);
+        }  
+       
+        
+      static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 10;
+
+            Console.WriteLine(a + "\t" + b);
+            calcsquare(a, b);
+            Console.WriteLine(a + "\t" + b);
+            Console.ReadLine();
+
+
+        }
+    }
+}
