4c0644333002b818073e1a1a96a3bdfa607f77b8
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-03 00:04:29 +0545

    Update function.cs

--- function.cs
+++ function.cs
@@ -1,12 +1,26 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp6
 {
-    class Program
+    class function
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+                int a, b, c;
+                
+
+                Console.WriteLine("Please enter value of a:");
+                a = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter value of b:");
+                b = Convert.ToInt32(Console.ReadLine());
+
+                Func<int, int, int> funcAdd = (a, b) => a + b;
+                c = funcAdd.Invoke(a, b);
+                Console.WriteLine(Convert.ToString(c));//Returns addition of two numbers entered by the user
+               Console.ReadLine();
+
+            
         }
     }
 }
