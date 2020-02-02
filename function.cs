c5ab5984c1b1ceb0d066d031f7dbe91af3c74732
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-02 23:58:55 +0545

    Add files via upload

--- function.cs
+++ function.cs
@@ -1,28 +1,12 @@
-﻿
-using System;
+﻿using System;
 
-namespace function
+namespace ConsoleApp6
 {
-    class function
+    class Program
     {
-        public static int add(int num1,int num2)
-        {
-            int result = num1 + num2;
-            return result;
-        }
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Enter first number:");
-            int x = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter second number:");
-            int y=Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Sum of two numbers:" + "\t" + add(x, y));
-            Console.ReadLine();
-
-
+            Console.WriteLine("Hello World!");
         }
-
-
     }
 }
